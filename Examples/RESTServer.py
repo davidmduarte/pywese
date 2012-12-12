cd25f7fc3715ec33f8bc17f11ecc6d4d6ce70704
David Duarte <davidmduarte@gmail.com>
2012-12-12 17:57:54 +0000

    80% done

--- Examples/RESTServer.py
+++ Examples/RESTServer.py
@@ -4,6 +4,7 @@ sporting = {
 	'team' : {
 		'players' : [
 			{
+				'id' : 0,
 				'name' : "Rui Patricio",
 				'position' : "Guarda Redes",
 				'stats' : {
@@ -37,18 +38,51 @@ def funcTeam(resquest):
 
 def funcCoach(request):
 	global sporting
-
 	if request['REQUEST_METHOD'] == 'GET':
-		return pywese.response(200, 'html/text', "Coach: " + sporting['coach'] + "<br>")
-	elif request['REUQEST_METHOD'] == 'POST':
-		sporting['coach'] = request['POST']['name']
+		return pywese.response(200, 'html/text', "Coach: " + sporting['team']['coach'] + "<br>")
+	elif request['REQUEST_METHOD'] == 'POST':
+		sporting['team']['coach'] = request['POST']['name']
 		return pywese.response(201, 'html/text', "done<br>/sporting/team/Coach GET to see")
-	elif request['REUQEST_METHOD'] == 'PUT':
-		sporting['coach'] += request['POST']['name']
+	elif request['REQUEST_METHOD'] == 'PUT':
+		sporting['team']['coach'] += request['POST']['name']
 		return pywese.response(200, 'html/text', "done<br>/sporting/team/Coach GET to see")
 	else:
-		sporting['coach'] = None 
+		sporting['team']['coach'] = None 
 		return pywese.response(200, 'html/text', "done<br>/sporting/team/Coach GET to see")
+
+def funcPlayers(request, playerId):
+	global sporting
+	if request['REQUEST_METHOD'] == 'GET':
+		return pywese.response(200, 'json', "players: " + str(sporting['team']['players']))
+	elif request['REQUEST_METHOD'] == 'POST':
+		sporting['team']['players'].append({
+			'id':  len(sporting['team']['players']) - 1
+			'name': request['POST']['name'],
+			'position': request['POST']['position'],
+			'stats' : {}
+		})
+		return pywese.response(201, 'html/text', "done")
+	elif request['REQUEST_METHOD'] == 'PUT':
+		if playerId == None:
+			return pywese.response(404, 'html/text', "Player Id needed")
+		else:
+			sporting['team']['players'][playerId]['nome'] = request['POST']['name']
+			sporting['team']['players'][playerId]['position'] = request['POST']['name']
+			return pywese.response(200, 'html/text', "")
+
+		for player in sporting['team']['players']:
+			if player['name'] == request['POST']['name']:
+				player['position'] = request['POST']['position']
+		return pywese.response(200, 'html/text', "done<br>/sporting/team/players GET to see")
+	else:
+		for player in sporting['team']['players']:
+			if player['name'] == request['POST']['name']:
+				sporting['team']['players'].remove(request['POST']['name'])
+				break
+		return pywese.response(200, 'html/text', "done<br>/sporting/team/Coach GET to see")
+
+def funcPLayersStats(request, playerId):
+	global sporting
 
 pywese.HttpServer("localhost", 8080, {
 	"^/sporting$": funcHelp,
